afd14250b69256f7457a7e63c072ae692ee7667e
KevinF42 <kforrister@segfault.com>
2018-02-08 08:37:21 -0800

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>siera.ioe </title>
+   <link rel="stylesheet" href="css/custom.css">
+	    <style>
+
+	img {
+    max-width: 100%;
+    height: 100px;
+}
+
+img:hover {
+     opacity: 0.5;
+}
+
+			#if-location {
+			position: absolute; left: 10px; top: 20px;
+			width:80px;
+			height:50px;
+		}
+
+		}
+		
+.one {
+    border-style: solid;
+    border-color: red;
+}
+
+.two {
+    border-style: solid;
+    border-color: green;
+} 
+
+.three {
+    border-style: solid;
+    border-color: red green blue yellow;
+} 
+
+</style>
+
+</head>
+<body>
+	<center>
+<a href="http://10.183.1.19:8080" target = "_blank" class = "menu" >:8080</a>
+<a href="http://10.183.1.19:3000" target = "_blank"class = "menu" >:3000</a>
+<a href="downloads/" class = "menu">downloads</a>
+<a href="pma/" class = "menu">pma</a>
+<a href="logs/" target = "_blank" class = "menu" >logs</a>
+<a href="contact/" target = "_blank" class = "menu" >contact</a>
+
+
+<br />
+<a href = "https://sites.google.com/a/tritechsc.org/kevinf/" target = "_blank"><img src = "images/googlelink.png"></a>
+<a href = "https://kevinf42.github.io/" target = "_blank"><img src = "images/github.png"></a>
+<a href = "https://github.com/KevinF42?tab=repositories" target = "_blank"><img src = "images/githubrepos.png"></a>
+<a href = "html/" target = "_blank"><img src = "images/html.png"></a>
+<a href = "nodejs/"><img src = "images/javascript.png"></a>
+<br />
+<a href = "app/" target = "_blank"><img src = "images/httpapp.png"></a>
+<a href = "php/" target = "_blank"><img src = "images/php.png"></a>
+<a href = "java/" target = "_blank"><img src = "images/java.png"></a>
+<a href = "code/" target = "_blank"><img src = "images/code.png"></a>
+<img src = "images/redsquare.png"></a>
+
+
+<br />
+
+
+
+</div>
+
+</pre>
+
+
+</body>
+</html>
